647e4dc64baff46c75549d1df96aa9e6f4974871
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-23 16:31:42 -0300

    refactor: move db authentication to folder

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,7 +2,7 @@ import {
   Authentication,
   authenticationModel,
 } from "../../../domain/usecases/authentication";
-import { LoadAccountByEmailRepository } from "../../protocols/load-account-by-email-repository";
+import { LoadAccountByEmailRepository } from "../../protocols/database/load-account-by-email-repository";
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
