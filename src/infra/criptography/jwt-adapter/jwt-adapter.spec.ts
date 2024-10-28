bfcda98d82dae2bde202a0545e9f3d247bccbcb4
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-10-28 20:39:14 -0300

    feat: ensure JwtAdapter return a token on sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from "jsonwebtoken";
 import { JwtAdapter } from "./jwt-adapter";
 
+jest.mock("jsonwebtoken", () => ({
+  async sign(): Promise<string> {
+    return new Promise((resolve) => resolve("any_token"));
+  },
+}));
+
 interface SutTypes {
   sut: JwtAdapter;
 }
@@ -21,5 +27,12 @@ describe("JWT Adapter", () => {
     await sut.encrypt("any_id");
 
     expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
+  });
+
+  test("Should return a token on sign success", async () => {
+    const { sut } = makeSut();
+    const accessToken = await sut.encrypt("any_id");
+
+    expect(accessToken).toBe("any_token");
   });
 });
